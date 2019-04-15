408eb3730d6c119cc9d6a57a4ad3094f5cd57f95
proyecto-JHO-BISOFT-04 <jhidalgoo@ucenfotec.ac.cr>
2019-04-15 10:50:52 -0600

    Update carrusel.js

--- public/js/carrusel.js
+++ public/js/carrusel.js
@@ -1,11 +1,9 @@
 'use strict';
 
   $('.noticias').slick({
-    centerPadding: '60px',
- 
     centerMode: true,
     centerPadding: '60px',
     slidesToShow: 3,
     autoplay: true,
-    autoplaySpeed: 9000,
+    autoplaySpeed: 9000
   });
