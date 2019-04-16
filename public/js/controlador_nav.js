1f0f1ba15151be8f3e1a8f2ae458560b5962680a
proyecto-JHO-BISOFT-04 <jhidalgoo@ucenfotec.ac.cr>
2019-04-16 03:13:35 -0600

    Update controlador_nav.js

--- public/js/controlador_nav.js
+++ public/js/controlador_nav.js
@@ -2,6 +2,9 @@
 
 const botonCerrarSesion = document.querySelector('#boton_cerrar');
 const lblNombreUsuario = document.querySelector('#lblNombreUsuario');
+const BtnMenuPalanca = document.querySelector('#btnMenuPalanca');
+const MenuOpciones = document.querySelector('#menuOpciones');
+let MenuOpcionesActivo = false;
 
 let controlar_navegacion = () => {
     let tipoUsuario = localStorage.getItem('tipoUsuario') || '';
@@ -34,9 +37,37 @@ let controlar_navegacion = () => {
         }
 };
 
+let fnBtnMenuPalanca = () => {
+	if (MenuOpcionesActivo) {
+		MenuOpciones.style = 'display: none;';
+		MenuOpcionesActivo = false;
+	} else {
+		MenuOpciones.style = 'display: block;';
+		MenuOpcionesActivo = true;
+	}
+};
+
+
+let menuNavVisible = (abierto) => {
+	if(true === abierto){
+		document.getElementById("mySidenav").style.width = "350px";
+   // document.getElementById("main").style.marginLeft = "350px";
+	}else{
+		 document.getElementById("mySidenav").style.width = "0";
+    //document.getElementById("main").style.marginLeft= "0";
+	}
+};
+
+
 controlar_navegacion();
 
 if (botonCerrarSesion) {
     botonCerrarSesion.addEventListener('click', cerrar_sesion, false);
 }
+
+if (BtnMenuPalanca) {
+	BtnMenuPalanca.addEventListener('click', fnBtnMenuPalanca, false);
+}
+
+
 
