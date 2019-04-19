d8995e86a589041c47756e23cbfc86550600aa79
jjimenezg19 <47197113+jjimenezg19@users.noreply.github.com>
2019-04-19 07:29:21 +0600

    Servicios registrar y listar
    commit 18_04_19_29

---
+++ api/componentes/servicios/servicios.model.js
@@ -0,0 +1,15 @@
+'use strict';
+/*se define el esquema de como se van a guardar los datos
+  de los servicios*/
+  const mongoose = require('mongoose');
+  const NombreTabla = 'servicio_';
+
+  let schema_servicio = new mongoose.Schema(
+      {
+        codigo : {type : mongoose.Schema.Types.Number, require : true, index : true},
+        nombre : {type : String, required : true},
+        descripcion : {type : String, required : true }
+      }
+  );
+
+  module.exports = mongoose.model(NombreTabla, schema_servicio);
