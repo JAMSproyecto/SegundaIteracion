14fa592a1ba5d9a3b6175f54374287fe3a868c93
Maguerov <maguerov@ucenfotec.ac.cr>
2019-04-20 18:17:23 -0600

    se revierten los cambios

--- public/js/controlador_registrar_lista_utiles.js
+++ public/js/controlador_registrar_lista_utiles.js
@@ -6,8 +6,10 @@ const input_anno = document.querySelector('#txt_anno');
 const titulo_centro = document.querySelector('#titulo_centro');
 const lista_centros = document.querySelector('#lista');
 const input_centros = document.querySelector('#lista_centros');
-const bloqueOcultar = document.querySelector('#bloqueCentrosOcultar');
-const bloqueOcultar2 = document.querySelector('#bloqueCentrosOcultar2');
+const label_centro = document.querySelector('#label_centro');
+input_tipo.classList.add('ocultar');
+input_centros.classList.add('ocultar');
+label_centro.classList.add('ocultar');
 
 
 let response = obtener_lista_utiles();
@@ -68,11 +70,10 @@ window.onload = () => {
     let tipoUsuario = localStorage.getItem('tipoUsuario');
 
     if (null !== tipoUsuario) {
-     
+
 
         if (tipoUsuario === 'SuperAdmin') {
             input_tipo.classList.remove('ocultar');
-            input_centros.classList.remove('ocultar');
             input_tipo.innerHTML = '<option value="">Seleccione el tipo de lista</option><option value="MEP">MEP</option><option value="centro_educativo">Centro Educativo</option>';
             input_tipo.selectedIndex = 0;
             cargarCEdu();
@@ -80,8 +81,11 @@ window.onload = () => {
             input_tipo.addEventListener('change', mostrar_centros);
 
         }else {
-            bloqueOcultar.classList.add('ocultar');
-            bloqueOcultar2.classList.add('ocultar');
+            input_tipo.innerHTML = '<option value="centro_educativo">Centro Educativo</option>';
+            input_tipo.selectedIndex = 0;
+            input_tipo.classList.add('ocultar');
+            let nombre = response.nombreCentro;
+            titulo_centro.innerHTML = nombre;
         }
     } else {
         console.error('No se encontró el tipo de usuario');
@@ -91,12 +95,13 @@ window.onload = () => {
   
 function mostrar_centros(){
     if(this.value != 'centro_educativo'){
-        localStorage.setItem('id', '1999');
-        bloqueOcultar.classList.remove('ocultar');
+        input_centros.classList.add('ocultar');
+        label_centro.classList.add('ocultar');
+        localStorage.setItem('id', '1999')
     }else{
-        bloqueOcultar.classList.add('ocultar');
+        input_centros.classList.remove('ocultar');
+        label_centro.classList.remove('ocultar');
         input_centros.addEventListener('change', obtener_codigo_centro);
-        
     }
 };
 
