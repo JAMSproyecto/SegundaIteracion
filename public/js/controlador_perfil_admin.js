84655fc76ca0ff05fd9c0d446dc35add14186955
Maguerov <maguerov@ucenfotec.ac.cr>
2019-04-21 14:07:13 -0600

    perfil admin

--- public/js/controlador_perfil_admin.js
+++ public/js/controlador_perfil_admin.js
@@ -1,53 +1,101 @@
 'use strict';
 
 const CardsCentros = document.querySelector('#cards_centros');
+const filtroCards = document.querySelector('#filtrar_cards');
+
+let irAlPerfil = (idCEdu) => {
+    localStorage.setItem('verPerfilCEdu', idCEdu);
+
+    const tipoUsuario = localStorage.getItem('tipoUsuario');
+    switch (tipoUsuario.toLowerCase()) {
+        case 'superadmin':
+            location.replace('./perfilCentroAdmin.html');
+            break;
+        case 'padrefamilia':
+            location.replace('./perfilCentroPadre.html');
+            break;
+        default:
+            console.error('El tipo de usuario no tiene página de redirección');
+            break;
+    }
+};
 
 let cargarCEdu = () => {
 
     listarCEdu((pSuccess, pMessage) => {
+
         if (pSuccess) {
-            if ('object' == typeof (pMessage)) {
-                    pMessage.forEach(obj => {
-                            let card = document.createElement('div');
-                            
-                            let centro_nombre = document.createElement('h1');
-                            centro_nombre.innerHTML = 'Nombre:' + obj['nombre'];
+            if ('object' == typeof pMessage) {
 
-                            let telefono = document.createElement('span');
-                            telefono.innerHTML = 'Teléfono: ' + obj['telefono'];
+                let filtros = filtroCards.value;
+                //Limpiamos antes de añadir los cards:
+                CardsCentros.innerHTML = '';
 
-                            let correo = document.createElement('span');
-                            correo.innerHTML = 'Correo: ' + obj['correo'];
+                pMessage.forEach(obj => {
 
-                            let provincia = document.createElement('span');
-                            let direccion = document.createElement('span');
-                            obj['direccion'].forEach(obj2 => {
-                                    provincia.innerHTML = 'Provincia: ' + obtenerProvinciaPorID(obj2['idProvincia']);
-                                    direccion.innerHTML = 'Dirección: ' + obj2['sennas'];
-                            });
+    
+                    if (obj['nombre'].toLowerCase().includes(filtros.toLowerCase())) {
 
-                            let verMas = document.createElement('a');
-                            verMas.addEventListener('click', () => {
-                                    irAlPerfil(obj['_id']);
-                            }, false);
-                            verMas.innerHTML = '<i class="fas fa-id-card"></i>';
+                        let card = document.createElement('div');
 
-                                card.appendChild(centro_nombre);
-                                card.appendChild(telefono);
-                                card.appendChild(correo);
-                                card.appendChild(provincia);
-                                card.appendChild(direccion);
-                                card.appendChild(verMas);
-                                
 
-                            CardsCentros.appendChild(card);
-                    });
 
+                        let centro_nombre = document.createElement('h1');
+                        centro_nombre.innerHTML = 'Nombre: ' + obj['nombre'];
+    
+                        let telefono = document.createElement('span');
+                        telefono.innerHTML = 'Teléfono: ' + obj['telefono'];
+    
+                        let correo = document.createElement('span');
+                        correo.innerHTML = 'Correo: ' + obj['correo'];
+    
+                        let provincia = document.createElement('span');
+                        provincia.innerHTML = 'Provincia: ' + obj['provincia'];
+    
+                        let direccion = document.createElement('span');
+                        direccion.innerHTML = 'Dirección: ' + obj['direccion'];
+    
+                        let calificacionMEP = document.createElement('p');
+    
+                        if ('string' == typeof obj['calificacionMEP'] && obj['calificacionMEP'].length > 0) {
+                            calificacionMEP.innerHTML = 'Calificación MEP: ' + obj['calificacionMEP'];
+                        }
+                        let verMas = document.createElement('a');
+                        verMas.addEventListener('click', () => {
+                            irAlPerfil(obj['_id']);
+                        }, false);
+                        verMas.innerHTML = '<i class="fas fa-id-card"></i>';
+    
+                        card.appendChild(centro_nombre);
+                        card.appendChild(telefono);
+                        card.appendChild(correo);
+                        card.appendChild(provincia);
+                        card.appendChild(direccion);
+                        card.appendChild(calificacionMEP);
+                        card.appendChild(verMas);
+    
+    
+                        CardsCentros.appendChild(card);
+
+
+                    };
+
+                    
+                });
+
+            } else {
+                CardsCentros.innerHTML = '';
+                console.error(pMessage);
             }
-    }
-});
+        } else {
+            CardsCentros.innerHTML = '<h4>' + pMessage + '</h4>';
+            console.error(pMessage);
+        }
+    });
 };
 
 window.onload = () => {
     cargarCEdu();
 };
+
+filtroCards.addEventListener('keyup', cargarCEdu);
