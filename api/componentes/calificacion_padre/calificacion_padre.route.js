2341fa550c473a8573491b3108b6c02ef90dafb9
Jei <jhidalgoo@ucenfotec.ac.cr>
2019-04-24 11:44:10 -0600

    Fix parametro en GET

--- api/componentes/calificacion_padre/calificacion_padre.route.js
+++ api/componentes/calificacion_padre/calificacion_padre.route.js
@@ -3,34 +3,43 @@ const Express = require('express');
 const Router = Express.Router();
 const API = require('./calificacion_padre.api');
 
-Router.param('id', (req, res, next, id) => {
-    req.body.id = id;
-    next();
+Router.param('id', (req, res, next, parametro) => {
+	try {
+		const n = parseInt(parametro, 10);
+		if (isNaN(n)) {
+			req.body.id = -1;
+		} else {
+			req.body.id = n;
+		}
+	} catch (e) {
+		req.body.id = -1;
+	}
+	next();
 });
 
 //Asignar calificación como padre a un centro educativo:
 Router.route('/asignar_calificacion_padre').post((req, res) => {
-    API.asignar_calificacion_padre(req.body, res);
+	API.asignar_calificacion_padre(req.body, res);
 });
 
 //buscar la información de calificación por el id:
 Router.route('/buscar_calificacion_padre_por_id/:id').get((req, res) => {
-    API.buscar_calificacion_padre_por_id(req.body.id, res);
+	API.buscar_calificacion_padre_por_id(req.body.id, res);
 });
 
 //buscar todas las calificaciones de a cuerdo al id del centro educativo:
 Router.route('/buscar_calificaciones_padre_por_idCentro/:id').get((req, res) => {
-    API.buscar_calificaciones_padre_por_idCentro(req.body.id, res);
+	API.buscar_calificaciones_padre_por_idCentro(req.body.id, res);
 });
 
 //Obtener todas las calificaciones que están asignadas:
 Router.route('/obtener_todas_calificaciones_padre').get((req, res) => {
-    API.obtener_todas_calificaciones_padre(req, res);
+	API.obtener_todas_calificaciones_padre(req, res);
 });
 
 //Recibe el id de la calificación que contiene el comentario que hay que actualizar y el comentario nuevo.
 Router.route('/actualizar_comentario_calificacion_padre').post((req, res) => {
-    API.actualizar_comentario_calificacion_padre(req.body, res);
+	API.actualizar_comentario_calificacion_padre(req.body, res);
 });
 
 /*
@@ -38,14 +47,14 @@ Router.route('/actualizar_comentario_calificacion_padre').post((req, res) => {
  * Recibe el id de la calificación que contiene el comentario que hay que eliminar.
 */
 Router.route('/eliminar_comentario_calificacion_padre/:id').get((req, res) => {
-    API.eliminar_comentario_calificacion_padre(req.body.id, res);
+	API.eliminar_comentario_calificacion_padre(req.body.id, res);
 });
 
 /*
  * Por medio del id obtiene la calificación total del ranking de los padres de familia. Es decir, el número de  estrellitas del centro educativo del 1 al 5.
 */
 Router.route('/obtener_calificacion_padre_de_centro/:id').get((req, res) => {
-    API.obtener_calificacion_padre_de_centro(req.body.id, res);
+	API.obtener_calificacion_padre_de_centro(req.body.id, res);
 });
 
 module.exports = Router;
