00a116906241ecad377275ebe2d823b63b7bd378
proyecto-JHO-BISOFT-04 <jhidalgoo@ucenfotec.ac.cr>
2019-04-24 00:51:01 -0600

    Add endpoint aprobar_centro_educativo

--- api/componentes/centro_educativo/centroEducativo.route.js
+++ api/componentes/centro_educativo/centroEducativo.route.js
@@ -1,27 +1,31 @@
 'use strict';
 const Express = require('express');
 const Router = Express.Router();
-const RegistrarCEduApi = require('./centroEducativo.api');
+const API = require('./centroEducativo.api');
 
-Router.param('id', (req, res, next, id) => {
-    req.body.id = id;
+Router.param('id', (req, res, next, parametro) => {
+    req.body.id = parametro;
     next();
 });
 
 Router.route('/registrar_centro_educativo').post((req, res) => {
-    RegistrarCEduApi.registrar_centro_educativo(req, res);
+    API.registrar_centro_educativo(req, res);
 });
 
 Router.route('/obtener_todos_centro_educativo').get((req, res) => {
-    RegistrarCEduApi.obtener_todos_centro_educativo(req, res);
+    API.obtener_todos_centro_educativo(req, res);
 });
 
 Router.route('/obtener_centros_educativos_sin_aprobar').get((req, res) => {
-    RegistrarCEduApi.obtener_centros_educativos_sin_aprobar(req, res);
+    API.obtener_centros_educativos_sin_aprobar(req, res);
 });
 
 Router.route('/obtener_centro_por_id/:id').get((req, res) => {
-    RegistrarCEduApi.obtener_centro_por_id(req, res);
+    API.obtener_centro_por_id(req, res);
+});
+
+Router.route('/aprobar_centro_educativo/:id').get((req, res) => {
+    API.aprobar_centro_educativo(req.body.id, res);
 });
 
 module.exports = Router;
